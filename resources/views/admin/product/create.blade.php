ceb03a14575e88fb7b083459e06bba76f64eee1e
rajesh-mondal <rajesh01749@gmail.com>
2023-05-25 02:22:30 +0600

    remove unnecessary error hadling code :fire:

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -30,17 +30,7 @@
             </div>
         </div><!-- /.container-fluid -->
     </section>
-
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
+    
     <!-- Main content -->
     <section class="content">
         <div class="container-fluid">
