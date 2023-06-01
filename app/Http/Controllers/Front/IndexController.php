3848485e724323deacaed3be521891e4aa97b21b
rajesh-mondal <rajesh01749@gmail.com>
2023-06-01 23:15:09 +0600

    banner product displayed on home page :zap:

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Product;
 use DB;
 
 class IndexController extends Controller
 {
     public function index(){
         $category = DB::table('categories')->get();
-        return view('frontend.index', compact('category'));
+        $banner_product = Product::where('product_slider', 1)->latest()->first();
+        return view('frontend.index', compact('category','banner_product'));
     }
 }
