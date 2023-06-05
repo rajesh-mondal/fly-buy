37ed77dfdaab81baa4f84f8c8c43a041acb64493
rajesh-mondal <rajesh01749@gmail.com>
2023-06-05 16:43:12 +0600

    single product details page done

--- resources/views/frontend/product_details.blade.php
+++ resources/views/frontend/product_details.blade.php
@@ -14,29 +14,52 @@
                     </ul>
                 </div>
 
-                <div class="col-lg-5 order-lg-2 order-1">
+                <div class="col-lg-4 order-lg-2 order-1">
                     <div class="image_selected"><img src="{{ asset('frontend/images/single_4.jpg')}}" alt=""></div>
                 </div>
 
-                <div class="col-lg-5 order-3">
+                <div class="col-lg-3 order-3">
                     <div class="product_description">
-                        <div class="product_category">Laptops</div>
-                        <div class="product_name">MacBook Air 13</div>
-                        <div class="rating_r rating_r_4 product_rating"><i></i><i></i><i></i><i></i><i></i></div>
-                        <div class="product_text">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum. laoreet turpis,
-                                nec
-                                sollicitudin dolor cursus at. Maecenas aliquet, dolor a faucibus efficitur, nisi tellus
-                                cursus urna,
-                                eget dictum lacus turpis.</p>
-                        </div>
+                        <div class="product_category">{{ $product->category->category_name }} > {{ $product->subcategory->subcategory_name }} </div>
+                        <div class="product_name" style="font-size: 20px">{{ $product->name }}</div>
+                        <div class="product_category"><b>Brand: {{ $product->brand->brand_name }} </b></div>
+                        {{-- <div class="rating_r rating_r_4 product_rating"><i></i><i></i><i></i><i></i><i></i></div> --}}
+                        @if($product->discount_price==NULL)
+                            <div class="product_price" style="margin-top: 25px;">{{ $setting->currency }}{{ $product->selling_price }}</div>
+                        @else
+                            <div class="product_price" style="margin-top: 25px;"><del class="text-danger h6">{{ $setting->currency }}{{ $product->selling_price }}</del>{{ $setting->currency }}{{ $product->discount_price }}</div>
+                        @endif
+                        {{-- <div class="product_text">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum. laoreet turpis, nec sollicitudin dolor cursus at. Maecenas aliquet, dolor a faucibus efficitur, nisi tellus cursus urna, eget dictum lacus turpis.</p>
+                        </div> --}}
                         <div class="order_info d-flex flex-row">
                             <form action="#">
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <label>Size</label>
+                                            <select class="form-control form-control-sm" name="size" id="">
+                                                <option value="">A</option>
+                                                <option value="">B</option>
+                                                <option value="">C</option>
+                                            </select>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <label>Color</label>
+                                            <select class="form-control form-control-sm" name="size" id="">
+                                                <option value="">A</option>
+                                                <option value="">B</option>
+                                                <option value="">C</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <br>
                                 <div class="clearfix" style="z-index: 1000;">
-
-                                    <div class="product_quantity clearfix">
+                                    <div class="product_quantity clearfix ml-2">
+                                        <!-- Product Quantity -->
                                         <span>Quantity: </span>
-                                        <input id="quantity_input" type="text" pattern="[0-9]*" value="1">
+                                        <input id="quantity_input" type="text" pattern="[1-9]*" value="1">
                                         <div class="quantity_buttons">
                                             <div id="quantity_inc_button" class="quantity_inc quantity_control"><i
                                                     class="fas fa-chevron-up"></i></div>
@@ -74,6 +97,9 @@
                             </form>
                         </div>
                     </div>
+                </div>
+                <div class="col-lg-3 order-4">
+                    
                 </div>
             </div>
         </div>
