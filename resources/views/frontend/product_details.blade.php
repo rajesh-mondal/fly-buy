8966e7eebb915de7e3e23ec2961fd727a429ceb3
rajesh-mondal <rajesh01749@gmail.com>
2023-06-09 00:37:34 +0600

    fix: Bug fixed in counting product ratings :bug:

--- resources/views/frontend/product_details.blade.php
+++ resources/views/frontend/product_details.blade.php
@@ -187,36 +187,38 @@
                             <div class="row">
                                 <div class="col-lg-3">
                                     Average Review of {{ $product->name }}:<br>
-                                    @if( intval($sum_rating/$count_rating) == 5)
-                                        <span class="fa fa-star checked"></span>
-                                        <span class="fa fa-star checked"></span>
-                                        <span class="fa fa-star checked"></span>
-                                        <span class="fa fa-star checked"></span>
-                                        <span class="fa fa-star checked"></span>
-                                    @elseif( intval($sum_rating/$count_rating) >= 4 && intval($sum_rating/5) < $count_rating )
-                                        <span class="fa fa-star checked"></span>
-                                        <span class="fa fa-star checked"></span>
-                                        <span class="fa fa-star checked"></span>
-                                        <span class="fa fa-star checked"></span>
-                                        <span class="fa fa-star"></span>
-                                    @elseif( intval($sum_rating/$count_rating) >= 3 && intval($sum_rating/5) < $count_rating )
-                                        <span class="fa fa-star checked"></span>
-                                        <span class="fa fa-star checked"></span>
-                                        <span class="fa fa-star checked"></span>
-                                        <span class="fa fa-star"></span>
-                                        <span class="fa fa-star"></span>
-                                    @elseif( intval($sum_rating/$count_rating) >= 2 && intval($sum_rating/5) < $count_rating )
-                                        <span class="fa fa-star checked"></span>
-                                        <span class="fa fa-star checked"></span>
-                                        <span class="fa fa-star"></span>
-                                        <span class="fa fa-star"></span>
-                                        <span class="fa fa-star"></span>
-                                    @else()
-                                        <span class="fa fa-star checked"></span>
-                                        <span class="fa fa-star"></span>
-                                        <span class="fa fa-star"></span>
-                                        <span class="fa fa-star"></span>
-                                        <span class="fa fa-star"></span>
+                                    @if( $sum_rating != NULL )
+                                        @if( intval($sum_rating/$count_rating) == 5)
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                        @elseif( intval($sum_rating/$count_rating) >= 4 && intval($sum_rating/5) < $count_rating )
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star"></span>
+                                        @elseif( intval($sum_rating/$count_rating) >= 3 && intval($sum_rating/5) < $count_rating )
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star"></span>
+                                            <span class="fa fa-star"></span>
+                                        @elseif( intval($sum_rating/$count_rating) >= 2 && intval($sum_rating/5) < $count_rating )
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star"></span>
+                                            <span class="fa fa-star"></span>
+                                            <span class="fa fa-star"></span>
+                                        @else()
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star"></span>
+                                            <span class="fa fa-star"></span>
+                                            <span class="fa fa-star"></span>
+                                            <span class="fa fa-star"></span>
+                                        @endif
                                     @endif
                                 </div>
 
